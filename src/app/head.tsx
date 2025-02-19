aad38dafb77c4ad3cd33d084143395961d0a7b4f
za-king <muhammadzakysyukur@gmail.com>
2025-02-19 22:16:39 +0700

    fix : add icon tabs 2

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Zaking Portfolio</title>
+      <meta name="description" content="Zaking portfolio website" />
+      <link rel="icon" href="/squirrel.svg" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    </>
+  );
+}
