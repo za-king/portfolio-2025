c0e0b8165f66403108ddfee23fb066cc44231c38
za-king <muhammadzakysyukur@gmail.com>
2025-02-19 23:09:35 +0700

    fix : remove domain redirect

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "firebasestorage.googleapis.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://zakingportfolio.my.id/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
